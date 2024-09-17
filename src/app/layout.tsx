0c08701593428bd4240c25b3776637c06eab9f6e
lizhagearty <lizhagearty@gmail.com>
2024-09-16 22:00:37 -0400

    adding icon for browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
@@ -55,6 +56,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
